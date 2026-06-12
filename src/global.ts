79a9a462b34aea057bf9eabffc4ce33a98b8b03e
Mohamed Goma <mohamed.goma.dev@gmail.com>
2026-06-12 12:14:42 +0300

    refactor: mapTracker.ts to make it scalable to update ui

--- src/global.ts
+++ src/global.ts
@@ -3,8 +3,8 @@ export const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 export const ctx = canvas.getContext("2d")!;
 export const margin = 20;
 
-export const ballsNumber = 5;
-export const ballSize = 40;
+export const ballsNumber = 10;
+export const ballSize = 10;
 export const ballRadius = ballSize / 2;
 
 export const minLimitX = ballRadius;
@@ -14,9 +14,9 @@ export const minLimitY = ballRadius;
 export const maxLimitY = canvas.height - ballRadius;
 
 export const speed = {
-  min: 3,
-  normal: 7,
-  max: 6,
+  min: 1,
+  normal: 1,
+  max: 3,
 };
 
 export const map = new Map<mapKeyType, mapValueType>();
--- src/scripts/mapTracker.ts
+++ src/scripts/mapTracker.ts
@@ -1,5 +1,5 @@
+import { distance } from "./../global";
 import { ctx, map } from "../global";
-import { distance } from "../global";
 import type { axises } from "../global";
 
 export default function mapTracker() {
@@ -8,11 +8,9 @@ export default function mapTracker() {
 
     map.forEach((ball) => {
       const { pos: pos2 } = ball;
-      if (distanceBetween(pos1, pos2) <= distance) {
-        ctx.beginPath();
-        ctx.moveTo(pos1.x, pos1.y);
-        ctx.lineTo(pos2.x, pos2.y);
-        ctx.stroke();
+      const diffDistance = distanceBetween(pos1, pos2);
+      if (diffDistance <= distance) {
+        drawLine(pos1, pos2);
       }
     });
   });
@@ -22,4 +20,11 @@ function distanceBetween(pos1: axises, pos2: axises): number {
   const axisX = pos2.x - pos1.x;
   const axisY = pos2.y - pos1.y;
   return Math.sqrt(Math.pow(axisX, 2) + Math.pow(axisY, 2));
+}
+
+function drawLine(pos1: axises, pos2: axises) {
+  ctx.beginPath();
+  ctx.moveTo(pos1.x, pos1.y);
+  ctx.lineTo(pos2.x, pos2.y);
+  ctx.stroke();
 }
